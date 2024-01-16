c731568df226b17e20f306b879ee8f4b55fe780d
endulum <71853916+endulum@users.noreply.github.com>
2024-01-16 14:46:53 -0800

    dynamic crosshair box, some readme notes

--- README.md
+++ README.md
@@ -6,4 +6,15 @@ Imitation of the classic *Where's Waldo?* game.
 ### Todo
 - [x] Basic page layout with scrollable box containing image and sticky headers/footers
 - [x] Correctly get the coordinates of a click on an image no matter its size
-- [ ] Visible box around click coordinates, that moves with each click
+- [x] Visible box around click coordinates, that moves with each click
+- [ ] 
+
+### Considering
+- Store coordinates as state and show them on the footer
+- Dropdown of characters and button in the footer to "check work" when locating a character
+- Blur mapbox before starting? 
+  - Dialog modal on startup to confirm starting the game
+  - On start, record time since start and show it on the header
+- More than one map
+  - Dialog modal that offers a map selection and changes the map
+  - Keep track of which maps have been cleared (local storage)
--- src/components/WaldoMap.jsx
+++ src/components/WaldoMap.jsx
@@ -1,18 +1,25 @@
+import { useRef } from 'react';
+
 import ImgUrl from '../assets/waldo1.jpg';
 // dimensions: 2444 * 1525
 
 export default function WaldoMap() {
+  const crosshairRef = useRef();
   function getCoords(e) {
     const bounds = e.target.getBoundingClientRect();
     const x = e.pageX - bounds.left - window.scrollX;
     const y = e.pageY - bounds.top - window.scrollY;
     const clickX = (x / e.target.clientWidth) * e.target.naturalWidth;
     const clickY = (y / e.target.clientHeight) * e.target.naturalHeight;
-    console.log(clickX, clickY);
+    // console.log(clickX, clickY);
+    crosshairRef.current.style.opacity = 1;
+    crosshairRef.current.style.top = `${clickY - 25}px`;
+    crosshairRef.current.style.left = `${clickX - 25}px`;
   }
 
   return (
     <div className="waldo-map">
+      <div className="crosshair" ref={crosshairRef} />
       <img
         onClick={getCoords}
         className="waldo-map-img"
