39e5cc82082293ea8ca6677b2d777faa7b35f8cb
endulum <71853916+endulum@users.noreply.github.com>
2024-01-16 13:35:12 -0800

    can get click coordinates

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Imitation of the classic *Where's Waldo?* game.
 
 ### Todo
 - [x] Basic page layout with scrollable box containing image and sticky headers/footers
-- [ ] Correctly get the coordinates of a click on an image no matter its size
+- [x] Correctly get the coordinates of a click on an image no matter its size
+- [ ] Visible box around click coordinates, that moves with each click
--- src/components/WaldoMap.jsx
+++ src/components/WaldoMap.jsx
@@ -1,9 +1,24 @@
 import ImgUrl from '../assets/waldo1.jpg';
+// dimensions: 2444 * 1525
 
 export default function WaldoMap() {
+  function getCoords(e) {
+    const bounds = e.target.getBoundingClientRect();
+    const x = e.pageX - bounds.left - window.scrollX;
+    const y = e.pageY - bounds.top - window.scrollY;
+    const clickX = (x / e.target.clientWidth) * e.target.naturalWidth;
+    const clickY = (y / e.target.clientHeight) * e.target.naturalHeight;
+    console.log(clickX, clickY);
+  }
+
   return (
     <div className="waldo-map">
-      <img className="waldo-map-img" src={ImgUrl} alt="A Where's Waldo map." />
+      <img
+        onClick={getCoords}
+        className="waldo-map-img"
+        src={ImgUrl}
+        alt="A Where's Waldo map."
+      />
     </div>
   );
 }
